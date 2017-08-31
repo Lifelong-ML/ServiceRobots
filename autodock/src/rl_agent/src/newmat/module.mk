02db10ff4cc5a43e828d2f0de1895dcf99781cc2
asinugobi <oasinu24@gmail.com>
2017-08-30 22:44:07 -0400

    removed code

--- autodock/src/rl_agent/src/newmat/module.mk
+++
@@ -1,3 +0,0 @@
-SRCS += $(patsubst %, newmat/%.cc, newmat1 newmat2 newmat3 newmat4 newmat5 newmat6 newmat7 newmat8 newmatex bandmat submat myexcept cholesky evalue fft hholder jacobi newfft sort svd newmatrm newmat9)
-
-
